7972f78d331e9119b27d3f8d6516c14890e101b4
IsuiLugo <izulugo@gmail.com>
2022-05-01 13:20:38 -0500

    Add more class and create the first GUI

---
+++ src/Clases/Read.java
@@ -0,0 +1,6 @@
+package Clases;
+
+public class Read {
+    //Clase para leer el archivo
+    
+}
---
+++ src/Clases/Tokens.java
@@ -0,0 +1,20 @@
+package Clases;
+
+public class Tokens {
+    //Variables globales
+    String currentToken;
+    
+    //TOKENA
+    public String TokenA(String currenteSymbol){
+     currentToken = "";
+        switch (currenteSymbol) {
+            case "a":
+                
+                break;
+            default:
+               
+        }
+     
+     return currentToken;
+    }
+}
